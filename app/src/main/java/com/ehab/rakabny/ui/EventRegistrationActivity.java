9735a7f9e1227d86042921ab71c381342b898c5e
ehabhamdy <ehabhamdy2012@gmail.com>
2017-10-26 21:36:30 +0200

    mod: Display confirmation dialog for event registration

--- app/src/main/java/com/ehab/rakabny/ui/EventRegistrationActivity.java
+++ app/src/main/java/com/ehab/rakabny/ui/EventRegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.ehab.rakabny.ui;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -17,6 +18,7 @@ import com.ehab.rakabny.model.EventRegistrationInformation;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
+import com.yarolegovich.lovelydialog.LovelyStandardDialog;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -26,6 +28,7 @@ import java.util.Map;
 import butterknife.BindView;;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class EventRegistrationActivity extends BaseActivity {
 
@@ -74,7 +77,6 @@ public class EventRegistrationActivity extends BaseActivity {
         firstNameEditText.setText(user_username);
         emailEditText.setText(user_email);
 
-        Intent intent = getIntent();
         Bundle bundle = getIntent().getExtras();
         Event event = bundle.getParcelable(EventsActivity.EXTRA_EVENT_DETAILS);
         Picasso.with(this).load(event.bannerUrl).into(posterImageView);
@@ -90,8 +92,26 @@ public class EventRegistrationActivity extends BaseActivity {
     public void register(View view) {
         if (!validateForm()) {
             return;
+        }else {
+            /*Intent intent = new Intent(this, InformationActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);*/
+            new LovelyStandardDialog(this)
+                    .setTopColorRes(R.color.colorPrimary)
+                    .setButtonsColorRes(R.color.primary_dark)
+                    .setIcon(R.drawable.ic_info_black_48dp)
+                    .setTitle(R.string.registration_confirmation_dialog_title)
+                    .setMessage(R.string.registration_confirmation_dialog_message)
+                    .setPositiveButton(android.R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            saveEventData();
+                            Toast.makeText(getApplicationContext(), R.string.registration_confirmation_dialog_title, Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, null)
+                    .show();
         }
-        saveEventData();
     }
 
     private void saveEventData() {
